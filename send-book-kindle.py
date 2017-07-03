97d317e2891a96c026928b1c70b6c557b6dee3c1
xu.chen <xu.chen@@chiefclouds.com>
2017-07-03 19:14:16 +0800

    Update by some small thing

--- send-book-kindle.py
+++ send-book-kindle.py
@@ -24,27 +24,32 @@ last_month = str(last_month)
 
 # Single month to Doble month 
 if len(str(last_month)) == 1:
-    last_month = str(0)+last_month
+    last_month = str(0) + last_month
 
 # Get download URL
+"""
 url_half01 = 'http://www.infoq.com/cn/minibooks/download/architect-'
 url = [url_half01, now_year, last_month, '?bookFormat=mobi']
 url = ''.join(url)
+"""
+url = 'http://www.infoq.com/cn/minibooks/download/architect-%s%s?bookFormat=mobi' %(now_year, last_month))
 
 headers = { #伪装为浏览器抓取
         'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
     }
 
-r = requests.get(url, stream=True)
+#r = requests.get(url, stream=True)
+r = requests.get(url, auth=('chenxuwq@163.com', '@Lan13826358458'))
+
 if r.status_code == 200:
    # request = urllib2.Request(url,headers=headers)
     response = urllib.urlopen(url)
  #   response = StringIO(response.read())
     with open("architect.mobi","wb") as f:
         f.write(response.read())
-    #with open("architect.mobi", "wb") as code:
+    """#with open("architect.mobi", "wb") as code:
         #code.write(r.content)
 
    # with open(url, "wb") as code:
-        #code.write(date)
+        #code.write(date)"""
 
